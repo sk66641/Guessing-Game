244df271aa5c550f7c913a388d93bd802cae3f6a
Khushi Kalra <kalra.khushi12@gmail.com>
2024-02-24 23:27:07 +0530

    Added Functionality

--- script.js
+++ script.js
@@ -9,16 +9,36 @@ function fun(){
         para.innerHTML="Congratulations! You guessed correctly. It took you "+ count + " tries.";
         para.classList.remove("red-text");
         para.classList.add("green-text");
+        para.classList.remove("blue-text");
     }
     else{
-        para.classList.remove("green-text");
-        para.classList.add("red-text");
         if(guess<num){
             para.innerHTML= "Try again. Go higher!";
-
+            para.classList.remove("green-text");
+            para.classList.add("blue-text");
+            para.classList.remove("red-text");
         }
         else{
             para.innerHTML= "Try again. Go lower!";
+            para.classList.remove("green-text");
+            para.classList.add("red-text");
+            para.classList.remove("blue-text");
         }
     }
+    t = document.querySelector("#guesses");
+    trow = document.createElement("tr");
+    td = document.createElement("td");
+    content=document.createTextNode(guess);
+    td.appendChild(content);
+    trow.appendChild(td);
+    t.append(trow);
+    if(guess == num) trow.classList.add("correct-guess");
+    else{
+        if(guess<num) trow.classList.add("incorrect-guess-low");
+        else trow.classList.add("incorrect-guess-high");
+    }
+}
+
+function resetGame() {
+    location.reload();
 }
